52f6a8040def273181e41f38b14b57ac144e3655
Felipe <felipe6san@hotmail.com>
2025-04-07 22:00:34 -0300

    Lab-Desenvolvimento- 07/04

---
+++ Lab-DesenvolvimentoWeb/fatec-orders-nestjs/src/database/seeders/20250408002831-insert-brand.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {    
+    await queryInterface.bulkInsert(
+      'brand',
+      [
+        {
+          name: 'Garoto',
+        },
+        {
+          name: 'Coca-Cola',
+        },
+        {
+          name: 'Lacta',
+        },
+      ],
+    {}
+  );    
+},
+
+  async down (queryInterface) {    
+    await queryInterface.bulkDelete('brand', null, {});     
+  }
+};
